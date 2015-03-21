34132e6af51cea8845a3757a1401a1852f383054
Arca Artem <arca.artem@gmail.com>
2015-03-21 22:19:53 +0000

    Exercise 4 of 13

---
+++ my_first_async_io/my_first_async_io.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    var count = data.split('\n').length - 1;
+    console.log(count);
+});
